04f25d89a75e3838d8d6e67aa916d55e83a1023f
fabezio <fabezio@outlook.fr>
2020-11-29 11:04:01 +0100

    project started

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+
+app.set('view engine', "ejs")
+app.use(express.static('static'))
+
+app.get("/", (req, res) => {
+    res.render('index')
+})
+
+const disp = console.log
+
+app.listen(3000, () => {
+  disp('server started...')
+})
