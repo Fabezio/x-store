ef0e2dd39867ad50f56bb228c9af0decaf8653db
fabezio <fabezio@outlook.fr>
2020-12-04 23:04:13 +0100

    passort added - user authentication

--- app.js
+++ app.js
@@ -1,34 +1,53 @@
 const express = require('express')
 const session = require('express-session')
+const app = express()
 const bodyParser = require('body-parser')
 require('./utils/db.config')
-const authRoutes = require('./routes/authRoutes')
 const ejs = require('ejs')
 const apiUsersRouter = require('./routes/apiUserRoutes')
-const { RuleTester } = require('eslint')
+// const { RuleTester } = require('eslint')
+
+// User Authentication
+const authRoutes = require('./routes/authRoutes')
+const passport = require('passport')
+require('./utils/authStrategies/localStrategy')
+app.use(passport.initialize())
+app.use(passport.session())
 
 // session config
-const app = express()
 // app.set('trust proxy', 1) // trust first proxy
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}))
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+  })
+)
 
 // ejs config
 app.set('view engine', 'ejs')
 app.use(express.static('static'))
 
 app.get('/', (req, res) => {
-  const username = req.session.name
-  const views = req.session.views
-  console.log(req.session)
-  console.log(views)
+  // const username = req.session.name
+  // const views = req.session.views
+  req.session.views = (req.session.views || 0) + 1
+  console.log('user:', req.user)
+  // console.log(views)
   // if (!username) username = 'Fabezio'
-  res.render('index', { name: username || 'pretty unknown user', views: views || 0 })
+  res.render('index', {
+    name: req.user || 'pretty unknown user',
+    views: req.session.views
+  })
 })
+app.get('/login-success', (req, res) => {
+  return res.render('login-success')
+})
+app.get('/login-failed', (req, res) => {
+  return res.render('login-failed')
+})
+
 app.get('/api', (req, res) => {
   res.render('api/index')
 })
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const { addUser } = require('../modules/users/service/userService')
+
+const passport = require('passport')
 const {
   registerSchema
 } = require('../modules/users/validations/authValidation')
-const { joiErrorFormatter, mongooseErrorFormatter } = require('../utils/validationFormatter')
+const {
+  joiErrorFormatter,
+  mongooseErrorFormatter
+} = require('../utils/validationFormatter')
+
+// registration
 router.get('/register', (req, res) => {
   return res.render('register', { message: null, errors: {}, formData: {} })
 })
@@ -24,7 +31,7 @@ router.post('/register', async (req, res) => {
       })
     } else {
       // return res.render('register', { message: 'registration successful!' })
-      return res.render('register', {
+      return res.redirect('/login', {
         message: {
           type: 'success',
           body: 'Registration success'
@@ -34,7 +41,7 @@ router.post('/register', async (req, res) => {
       })
     }
 
-    const user = await addUser(req.body)
+    // const user = await addUser(req.body)
   } catch (err) {
     console.error(err)
     /* res.send(mongooseErrorFormatter(err)) */
@@ -53,5 +60,73 @@ router.post('/register', async (req, res) => {
   }
   // let message
 })
+
+// login
+router.get('/login', (req, res) => {
+  return res.render('login', { message: null, errors: {}, formData: {} })
+})
+
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/login-success',
+    failureRedirect: '/login-failed'
+    // failureFlash: true
+  }),
+  async (req, res) => {
+    return res.render('login', {
+      message: {
+        type: 'success',
+        body: 'Login success!'
+      },
+      errors: {},
+      formData: {}
+    })
+
+    // try {
+    //   const validationResult = registerSchema.validate(req.body, {
+    //     abortEarly: false,
+    //   })
+    //   if (validationResult.error) {
+    //     return res.render('register', {
+    //       message: {
+    //         type: 'error',
+    //         body: 'Validation errors',
+    //       },
+    //       errors: joiErrorFormatter(validationResult.error),
+    //       formData: req.body,
+    //     })
+    //   } else {
+    //     // return res.render('register', { message: 'registration successful!' })
+    //     return res.redirect('/login', {
+    //       message: {
+    //         type: 'success',
+    //         body: 'Registration success',
+    //       },
+    //       errors: {},
+    //       formData: req.body,
+    //     })
+    //   }
+
+    //   // const user = await addUser(req.body)
+    // } catch (err) {
+    //   console.error(err)
+    //   /* res.send(mongooseErrorFormatter(err)) */
+    //   /* return res
+    //     .status(400)
+
+    //     .render('register', { message: 'something went wrong...' }) */
+    //   return res.status(400).render('register', {
+    //     message: {
+    //       type: 'error',
+    //       body: 'Validation errors',
+    //     },
+    //     errors: mongooseErrorFormatter(err),
+    //     formData: req.body,
+    //   })
+    // }
+    // // let message
+  }
+)
 
 module.exports = router
